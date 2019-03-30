0345d769b54fc4b16ce3275914bdf8adea0b7383
Meli Cordero <melisacorderoarias@hotmail.com>
2019-03-29 18:23:55 -0600

    R3

--- PoyectoAseguramiento.py
+++ PoyectoAseguramiento.py
@@ -57,3 +57,26 @@ def fecha_es_valida(tupla):
         return False
 
 
+def dia_siguiente(fecha):
+    if fecha [1] == 1 or fecha [1] == 3 or fecha [1] == 5 or fecha[1] == 7 or fecha[1] == 8 or fecha[1] == 10:
+        if fecha[2] < 31:
+            return (fecha[0], fecha [1], fecha[2] + 1)
+        return (fecha[0], fecha[1] + 1, 1)
+    if fecha [1] == 2:
+        if fecha[2] < 28:
+            return (fecha[0], 2, fecha [2] + 1)
+        else:
+            if fecha[2] == 28 and bisiesto(fecha[0]):
+                return (29, 2, fecha[2])
+            return (fecha[0], 3, 1)
+    if fecha[1] == 4 or fecha[1] == 6 or fecha [1] == 9 or fecha[1] ==11:
+        if fecha[2] < 30:
+            return (fecha[0], fecha [1], fecha[2] + 1)
+        return (1, fecha[1] + 1, fecha[2])
+    if fecha[1] == 12:
+        if fecha[2] < 31:
+            return (fecha[0], fecha [1], fecha[2] + 1)
+        return (fecha[0] + 1, 1, 1)
+            
+
+
